e6e0f63979dcc338cc5aa0f8913fbaa734714477
Juanma-cardona <juan.cardona475@pascualbravo.edu.co>
2026-09-09 19:39:31 -0500

    first commit

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import { defaultExclude, defineConfig } from "vitest/config";
+
+export default defineConfig({
+test: {
+    environment: "node",
+    globals: true,
+    include: ["test/**/*.spec.ts"],
+    coverage: {
+        provider: "v8",
+        reporter: ["text", "html"],
+        include: ["src/**/*.ts"],
+        exclude: [
+            "**/*spec.ts",
+            "**node_modules/**",
+            "src/config/**",
+            "src/services/interfaces/**",
+            "src/index.ts",
+            "src/entities/**",
+        ]
+    }
+}
+});
